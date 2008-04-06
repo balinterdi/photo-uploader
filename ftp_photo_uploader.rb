2f8bdeaa0358fb68d2aa1eba3875a944f3025582
Balint Erdi <erdibalint@freemail.hu>
2008-04-06 21:16:47 +0200

    get_photo_rel_path added and tested
    Signed-off-by: Balint Erdi <erdibalint@freemail.hu>

--- ftp_photo_uploader.rb
+++ ftp_photo_uploader.rb
@@ -6,12 +6,19 @@ class PhotoSorter
 
   def initialize
     @photo_data = nil
+    @extension = 'jpg'
   end
 
   def get_photo_data(yaml_file)
     @yaml_file = yaml_file
     photo_yaml_data = File.open(@yaml_file) { |f| f.read }
     @photo_data = YAML.load(photo_yaml_data)
+  end
+
+  def get_photo_rel_path(dir_name, prefix, file_name)
+    dir_name = '.' if dir_name.empty?
+    file_name = prefix + file_name unless prefix.nil?
+    "#{dir_name}/#{file_name}.#{@extension}"
   end
 
   def expand_photo_names
@@ -24,7 +31,8 @@ class PhotoSorter
           photo_fname_and_num_copies = photo_name.to_s.split('?')
           photo_fname = photo_fname_and_num_copies[0]
           num_copies = photo_fname_and_num_copies.length > 1 ? photo_fname_and_num_copies[1] : '1'
-          expanded_photo_names[num_copies] = {} unless expanded_photo_names.key?(num_copies)
+          expanded_photo_names[num_copies] = [] unless expanded_photo_names.key?(num_copies)
+          expanded_photo_names[num_copies] << get_photo_rel_path(dir_name, prefix, photo_fname)
           # puts "XXX expanded_photo_names: #{expanded_photo_names.inspect}"
         end
       end
--- ftp_photo_uploader_test.rb
+++ ftp_photo_uploader_test.rb
@@ -8,6 +8,13 @@ class FtpPhotoUploaderTest < Test::Unit::TestCase
     @photo_sorter.photo_data = {"petrababa"=>[{"files"=>["23?2", "56?4", 89, 90], "with"=>"dsc543"}], "newyork"=>[{"files"=>["02?3", "03", "07", "13?2"], "with"=>"dsc024"}, {"files"=>[78, "84?2"], "with"=>"dsc037"}, {"files"=>["dsc84934", "dsc746673?3"]}]}
   end
 
+  def test_get_photo_rel_path
+    assert_equal('newyork/dsc01245.jpg', @photo_sorter.get_photo_rel_path('newyork', 'dsc012', '45'))
+    assert_equal('newyork/dsc034745.jpg', @photo_sorter.get_photo_rel_path('newyork', nil, 'dsc034745'))
+    assert_equal('./dsc034745.jpg', @photo_sorter.get_photo_rel_path('', 'dsc034', '745'))
+    assert_equal('./dsc034745.jpg', @photo_sorter.get_photo_rel_path('', nil, 'dsc034745'))
+  end
+
   def test_expands_photo_names_well
     photo_names = @photo_sorter.expand_photo_names
     assert(photo_names.key?('1'))
